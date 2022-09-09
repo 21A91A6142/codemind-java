101f7a05b0c813087b5965584573106a01696f4e
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-09 11:43:50 +0530

    import java.util.Scanner; class input {     public static void main(String args[])     {         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.println(s);         sc.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class input
+{
+    public static void main(String args[])
+    {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.println(s);
+        sc.close();
+    }
+}
