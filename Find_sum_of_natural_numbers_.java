afbc299f05de54cf9ee1c770931b84200ed6c264
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-18 10:38:24 +0530

    import java.util.*; class Natural {     public static void main(String args[])     {         int a,n;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         n=(a*(a+1))/2;         System.out.print(n);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Natural
+{
+    public static void main(String args[])
+    {
+        int a,n;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        n=(a*(a+1))/2;
+        System.out.print(n);
+    }
+}
