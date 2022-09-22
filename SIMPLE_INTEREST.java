0a8a1d888810ac402bf9c8467f7c1c68a89de8a5
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-22 12:07:48 +0530

    import java.util.*; class Simple {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         System.out.print((p*t*r)/100);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Simple
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        System.out.print((p*t*r)/100);
+    }
+}
