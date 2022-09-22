8a2fe8b0b6d4753ae2a787c3b729a6558ba253b8
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-22 15:44:17 +0530

    import java.util.*; class Diagonal {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         System.out.print(((a)*(a-3))/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Diagonal
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        System.out.print(((a)*(a-3))/2);
+    }
+}
