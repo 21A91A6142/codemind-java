ce68b660013f2da01a213e6a44d3f04928f285e6
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-23 22:04:18 +0530

    import java.util.*; class Last {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j=0,k=0;         n=sc.nextInt();         int [] a=new int[n];         int [] b=new int[100];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]%2==0)             {                 b[j]=a[i];                 j+=1;             }         }         System.out.print(b[j-1]);     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Last
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j=0,k=0;
+        n=sc.nextInt();
+        int [] a=new int[n];
+        int [] b=new int[100];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                b[j]=a[i];
+                j+=1;
+            }
+        }
+        System.out.print(b[j-1]);
+    }
+}
