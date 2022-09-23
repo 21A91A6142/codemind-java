b14b1794c27d1cfb61a535f193b909c1e1398850
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-23 20:44:24 +0530

    import java.util.*; class Array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,avg,c=0;         n=sc.nextInt();         int [] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             s=s+a[i];         }         avg=s/n;         for(i=0;i<n;i++)         {             if(a[i]<=avg)             {                 c=c+1;             }         }         System.out.print(c);
        }
    }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,avg,c=0;
+        n=sc.nextInt();
+        int [] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            s=s+a[i];
+        }
+        avg=s/n;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]<=avg)
+            {
+                c=c+1;
+            }
+        }
+        System.out.print(c);
+        
+        
+    }
+}
