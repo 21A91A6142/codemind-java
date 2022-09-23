a28dc66f6a20fdf79207e9735192bdd5b08c1614
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-23 22:29:24 +0530

    import java.util.*; class Array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s,avg,c=0;         n=sc.nextInt();         int [] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         s=a[0];         for(i=0;i<n;i++)         {             if(s<a[i])             {                 s=a[i];             }         }         System.out.print(s);
        }
    }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s,avg,c=0;
+        n=sc.nextInt();
+        int [] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        s=a[0];
+        for(i=0;i<n;i++)
+        {
+            if(s<a[i])
+            {
+                s=a[i];
+            }
+        }
+        System.out.print(s);
+        
+        
+    }
+}
