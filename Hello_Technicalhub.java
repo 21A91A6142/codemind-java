88fd85320556fbe696ebcb5dfdee7146c61a09b7
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-05 13:10:23 +0530

    import java.util.*; class Hello {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Hello
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s);
+    }
+}
