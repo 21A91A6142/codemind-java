4633b604836891ade8e70e03702bd092e33bbf8d
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-05 12:55:47 +0530

    import java.util.*; class Lower {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String s1=s.toLowerCase();         System.out.print(s1);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Lower
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String s1=s.toLowerCase();
+        System.out.print(s1);
+    }
+}
