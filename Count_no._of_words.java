0ade46b24f674615c11757f4a43ee52da6428521
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-05 11:12:36 +0530

    import java.util.*; class Count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s2[]=s1.split(" ");         System.out.print(s2.length);     } }

---
+++ Count_no._of_words.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2[]=s1.split(" ");
+        System.out.print(s2.length);
+    }
+}
