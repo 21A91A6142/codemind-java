d4cb9348f7d2d1ad3e8e0f7fd0c94cc083854b7b
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-06 18:07:01 +0530

    import java.util.*; class Count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] a=new int[n];         int c=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=1;i<n-1;i++)         {             if(a[i-1]%2!=0 && a[i+1]%2!=0)             {                 if(a[i]%2==0)                 {                     c=c+1;                 }             }         }         System.out.print(c);     } }

--- count_even_numbers_between_odd_numbers.java
+++ count_even_numbers_between_odd_numbers.java
@@ -3,19 +3,19 @@ class Count
 {
     public static void main(String args[])
     {
-        int n,i,c=0;
         Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
+        int n=sc.nextInt();
         int [] a=new int[n];
-        for(i=0;i<n;i++)
+        int c=0;
+        for(int i=0;i<n;i++)
         {
             a[i]=sc.nextInt();
         }
-        for(i=1;i<n-1;i++)
+        for(int i=1;i<n-1;i++)
         {
-            if(a[i]%2==0)
+            if(a[i-1]%2!=0 && a[i+1]%2!=0)
             {
-                if(a[i-1]%2!=0 && a[i+1]%2!=0)
+                if(a[i]%2==0)
                 {
                     c=c+1;
                 }
