41a359bec193d3b0013747b9aa8f6112c58758a7
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-06 17:54:16 +0530

    import java.util.*; class Strictly {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int c=0,m=0;         int n=sc.nextInt();         int [] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(a[i]%2==0)             {                 c+=1;                 if(i%2==0)                 {                     m=m+1;                 }                 else                 {                     break;                 }             }         }         if(c==m)         {             System.out.print("True");         }         else         {             System.out.print("False");         }     } }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Strictly
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int c=0,m=0;
+        int n=sc.nextInt();
+        int [] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                c+=1;
+                if(i%2==0)
+                {
+                    m=m+1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        if(c==m)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+            System.out.print("False");
+        }
+    }
+}
