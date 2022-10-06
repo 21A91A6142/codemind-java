35c1aa2d32e25334e151d3901c8f73d4242c2567
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-06 18:53:05 +0530

    import java.util.*; class Strictly {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int s=0,s1=0;         int n=sc.nextInt();         int [] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n/2;i++)         {             s+=a[i];         }         for(int i=(n/2);i<n;i++)         {             s1+=a[i];         }         System.out.println(s);         System.out.println(s1);     } }

---
+++ sum_of_first_half_and_second_half_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Strictly
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=0,s1=0;
+        int n=sc.nextInt();
+        int [] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n/2;i++)
+        {
+            s+=a[i];
+        }
+        for(int i=(n/2);i<n;i++)
+        {
+            s1+=a[i];
+        }
+        System.out.println(s);
+        System.out.println(s1);
+    }
+}
