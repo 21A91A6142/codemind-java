db3e87c2a16937d42f183133cab444cc74f52a2e
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-16 15:55:53 +0530

    import java.util.*; import java.util.ArrayList; class Unique {     static int isunique(int k,int a[])     {         int c=0;         for(int i=0;i<a.length;i++)         {             if(a[i]==k)             {                 c=c+1;             }         }         return c;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         ArrayList<Integer> list=new ArrayList<Integer>();         Set<Integer> slist=new HashSet<Integer>();         int n=sc.nextInt();         int []a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();             slist.add(a[i]);         }         //System.out.print(list);         int f,c=0;         for(int i:slist)         {             if(i%2==0)             {                 c=c+1;             }         }         System.out.print(c);     } }

---
+++ unique_even_count.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.ArrayList;
+class Unique
+{
+    static int isunique(int k,int a[])
+    {
+        int c=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==k)
+            {
+                c=c+1;
+            }
+        }
+        return c;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        Set<Integer> slist=new HashSet<Integer>();
+        int n=sc.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            slist.add(a[i]);
+        }
+        //System.out.print(list);
+        int f,c=0;
+        for(int i:slist)
+        {
+            if(i%2==0)
+            {
+                c=c+1;
+            }
+        }
+        System.out.print(c);
+    }
+}
