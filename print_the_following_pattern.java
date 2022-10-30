67feb4378fd5fbbf86825b090676ab10eb001e6b
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-30 09:45:03 +0530

    import java.util.*; class pat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(i==j)                 {                     System.out.print("0");                 }                 else                 {                     System.out.print("x");                 }             }             System.out.println("");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,22 @@
 import java.util.*;
-class patt
+class pat
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=n;i>0;i--)
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=0;j<n;j++)
             {
-                System.out.print(j);
+                if(i==j)
+                {
+                    System.out.print("0");
+                }
+                else
+                {
+                    System.out.print("x");
+                }
             }
             System.out.println("");
         }
