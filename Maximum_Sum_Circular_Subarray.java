ac0951432a6b0539c67170cc146d6d1d91e4a89c
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-12-01 14:28:55 +0530

    import java.util.*; class maxsumcir {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         ArrayList<Integer> l=new ArrayList<>();         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();             l.add(a[i]);         }         for(int i=0;i<n-1;i++)         {             for(int j=i+1;j<n;j++)             {                 int s=0;                 for(int k=i;k<j+1;k++)                 {                     s+=a[k];                 }                 l.add(s);             }         }         l.add(a[n-1]+a[0]);         System.out.print(Collections.max(l));     } }

---
+++ Maximum_Sum_Circular_Subarray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class maxsumcir
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> l=new ArrayList<>();
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            l.add(a[i]);
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int s=0;
+                for(int k=i;k<j+1;k++)
+                {
+                    s+=a[k];
+                }
+                l.add(s);
+            }
+        }
+        l.add(a[n-1]+a[0]);
+        System.out.print(Collections.max(l));
+    }
+}
